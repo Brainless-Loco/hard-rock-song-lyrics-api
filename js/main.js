e92a052c516a04bcb10925bd9ac67210adea5a07
Brainless-Loco <tonmoydas961179@gmail.com>
2021-02-07 03:13:31 +0600

    footer add and other simple changings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log(55);
+const searchBar = document.getElementById("search-ber");
+searchBar.addEventListener("input",function(e){
+    let inputObject = e.target.value;
+    console.log(inputObject);
+})
+var x = document.getElementById("search-result");
+x.innerHTML = "";
