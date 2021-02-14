89fcf8dff0f8df7b08ffd9a4900f01304cda480f
Brainless-Loco <tonmoydas961179@gmail.com>
2021-02-14 22:58:27 +0600

    album link and artist link issue fixed

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ searchBar.addEventListener("input",function(e){
         //  console.log(arrayOfObjects.data.length);
         // Got Data
         let Data = arrayOfObjects.data;
-        // console.log(arrayOfObjects);
+        console.log(arrayOfObjects);
         for(let i = 0; i<Data.length;i++){
             const songName = Data[i].title;
             const albumName = Data[i].album.title;
@@ -47,7 +47,7 @@ searchBar.addEventListener("input",function(e){
             <div id="resultno${i}" class="single-result row align-items-center my-3 p-3">
                 <div class="col-md-9 mb-2">
                     <h3 class="h3">${songName}<h3>
-                    <p class="author lead h3">From <span class="" font-weight-bold">${albumName}<br>by <a class="lyrics-name text-warning font-weight-bold" herf="${artistLink}">${artistName}</a></span></p>
+                    <p class="author lead h3">From <span class="font-weight-bold"><a href="${fullAlbum}" class="text-white"> ${albumName}&nbsp; <i class="fas fa-external-link-alt h6"></i></a><br>by <a class="lyrics-name text-warning font-weight-bold" href="${artistLink}">${artistName}</a></span></p>
                 </div>
                 <div class="col-md-3 mb-2 text-md-right text-center">
                     <button class="btn btn-success" onclick="showDetails(this.id)" id="get-details${i}">Get Details</button>
@@ -59,23 +59,27 @@ searchBar.addEventListener("input",function(e){
             <div class="col-8 text-center mx-auto">
                 <img src="${albumCover}" class="img-fluid sized_img" alt="">
             </div>
+            <br>
+            <div class="col-8 text-center mx-auto">
+                <audio controls>
+                    <source src="${Data[i].preview}" type="audio/ogg">
+                </audio>
+            </div>
             <div class="col-12 py-3">
                 <div class="col-5 mx-auto text-center">
-                    <a href="${songLink}" class="text-white font-weight-bold h4" target="_blank">Listen Now</a>
+                    <a href="${songLink}" class="text-white font-weight-bold h4" target="_blank"><i class="fas fa-music"></i> Listen Full Song <i class="fas fa-music"></i></a>
                     <p>${durationConverter(duration)}</p>
                     <h3>Lyrics</h3>
                     <hr style="background-color:white;">
                 </div>
             </div>
             <pre id="lyricsno${i}" class="lyric text-center col-12 text-white"></pre>
-            <div class="col-12">
-                <a href="${fullAlbum}" class="text-white font-weight-bold h4" target="_blank">Full Album</a>
-            </div>`;
+            `;
             searchResult.innerHTML += markUp;
             document.getElementById("detailsno"+i).innerHTML = markup2;
             getLyrics(artistName,songName,i);
             // console.log(getLyrics(artistName,songName));
-            if(i==6){
+            if(i==9){
                 break;
                 // not more than 7 songs
             }
